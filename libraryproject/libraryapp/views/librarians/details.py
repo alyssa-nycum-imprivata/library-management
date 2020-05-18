7b84d8b46f7dd214f9b2dc2ba1f5f916cc3b0bac
Alyssa Nycum <alyssa.nycum@gmail.com>
2020-05-18 11:02:34 -0500

    changed librarian views to use ORM

--- libraryproject/libraryapp/views/librarians/details.py
+++ libraryproject/libraryapp/views/librarians/details.py
@@ -6,25 +6,10 @@ from libraryapp.models import Librarian
 from ..connection import Connection
 
 def get_librarian(librarian_id):
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = sqlite3.Row
+    
+    librarian = Librarian.objects.get(pk=librarian_id)
 
-        db_cursor = conn.cursor()
-
-        db_cursor.execute("""
-        select
-            l.id,
-            l.library_id,
-            l.user_id,
-            u.first_name,
-            u.last_name,
-            u.email
-        from libraryapp_librarian l
-        join auth_user u on l.user_id = u.id
-        WHERE l.id = ?
-        """, (librarian_id,))
-
-        return db_cursor.fetchone()
+    return librarian
 
 @login_required
 def librarian_details(request, librarian_id):
--- libraryproject/libraryapp/views/librarians/list.py
+++ libraryproject/libraryapp/views/librarians/list.py
@@ -6,35 +6,8 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def librarian_list(request):
-    with sqlite3.connect(Connection.db_path) as conn:
-        conn.row_factory = sqlite3.Row
-        db_cursor = conn.cursor()
-
-        db_cursor.execute("""
-        select
-            l.id,
-            l.library_id,
-            l.user_id,
-            u.first_name,
-            u.last_name,
-            u.email
-        from libraryapp_librarian l
-        join auth_user u on l.user_id = u.id
-        """)
-
-        all_librarians = []
-        dataset = db_cursor.fetchall()
-
-        for row in dataset:
-            librarian = Librarian()
-            librarian.id = row["id"]
-            librarian.library_id = row["library_id"]
-            librarian.user_id = row["user_id"]
-            librarian.first_name = row["first_name"]
-            librarian.last_name = row["last_name"]
-            librarian.email = row["email"]
-
-            all_librarians.append(librarian)
+    
+    all_librarians = Librarian.objects.all()
 
     template_name = 'librarians/list.html'
 
